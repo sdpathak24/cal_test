8255fcd91ab3bb8f3ae93979ea46f93c0a58a388
sdpathak24 <83827570+sdpathak24@users.noreply.github.com>
2021-11-24 12:23:03 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -36,8 +36,7 @@ const months = [
 document.querySelector('.date h1').innerHTML
 =months[date.getMonth()];
 
-document.querySelector('.date p').innerHTML
-=date.toDateString();
+document.querySelector(".date p").innerHTML = new Date().toDateString();
 
 let days = "";
 
